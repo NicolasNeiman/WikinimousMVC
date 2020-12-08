3a7e48f6ed4ce177a1080c3f2e0561169d4bcd32
Nicolas Neiman <nicolasneiman@gmail.com>
2020-12-08 15:06:04 +0100

    create DB context class

---
+++ Data/WikinimousMVCContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WikinimousMVC.Models;
+
+namespace WikinimousMVC.Data
+{
+    public class WikinimousMVCContext : DbContext
+    {
+        public WikinimousMVCContext (DbContextOptions<WikinimousMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Post> Post { get; set; }
+    }
+}
