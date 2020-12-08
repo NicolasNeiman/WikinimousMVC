126cc15e1cf6526e713d99e4f8f6cb88837cc8c7
Nicolas Neiman <nicolasneiman@gmail.com>
2020-12-08 18:25:16 +0100

    Seed

---
+++ Models/SeedData.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WikinimousMVC.Data;
+using System;
+using System.Linq;
+
+namespace WikinimousMVC.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new WikinimousMVCContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<WikinimousMVCContext>>()))
+            {
+                // Look for any Posts.
+                if (context.Post.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Post.AddRange(
+                    new Post
+                    {
+                        Title = "Apple goes bankrupt",
+                        PostDate = DateTime.Parse("2020-12-07"),
+                        Content = "This is the most surprising news of this very strange year ..."
+                    },
+
+                    new Post
+                    {
+                        Title = "Criteo takes over Amazon",
+                        PostDate = DateTime.Parse("2020-12-07"),
+                        Content = "Not a surprise given the talents at Criteo ..."
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
