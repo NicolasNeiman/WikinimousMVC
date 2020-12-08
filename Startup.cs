37279c9d185035c35a47b34d09f5d9cdfe2940f6
Nicolas Neiman <nicolasneiman@gmail.com>
2020-12-08 15:31:32 +0100

    update default controller to Post - Create Post controller with an Index - Create Index view

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace WikinimousMVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Post}/{action=Index}/{id?}");
             });
         }
     }
